4ee8260eccde761113c432fe46b4cbaf99bf2d99
amribeiror <andrefromportugal@gmail.com>
2017-03-03 01:08:33 +0100

    Add files via upload

--- bash_script.sh
+++ bash_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir -p PROJECTS/project_one/{scripts,input,output,logs} datasets general_scripts paper
+mv "membrane-beta_2state.3line.txt" datasets
+mv project_andre_rosa.py PROJECTS/project_one/scripts
 
-mv ./membrane-beta_2state.3line.txt ./datasets/
-mv ./project_andre_rosa/ ./PROJECTS/project_one/scripts
